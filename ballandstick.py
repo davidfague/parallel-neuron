2265fc8850915936c6231de79a861aebd8b06b31
David Fague <98128717+davidfague@users.noreply.github.com>
2025-03-07 13:02:41 -0600

    Update generic Cell class in ballandstick.py

--- ballandstick.py
+++ ballandstick.py
@@ -1,11 +1,39 @@
 class Cell:
-    def __init__(self, gid):
+    def __init__(self, gid, x, y, z, theta):
         self._gid = gid
         self._setup_morphology()
         self.all = self.soma.wholetree()
         self._setup_biophysics()
+        self.x = self.y = self.z = 0                     # <-- NEW
+        h.define_shape()
+        self._rotate_z(theta)                            # <-- NEW        
+        self._set_position(x, y, z)                      # <-- NEW
+        
     def __repr__(self):
         return '{}[{}]'.format(self.name, self._gid)
+    
+    # everything below here is NEW
+    
+    def _set_position(self, x, y, z):
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                sec.pt3dchange(i,
+                               x - self.x + sec.x3d(i),
+                               y - self.y + sec.y3d(i),
+                               z - self.z + sec.z3d(i),
+                              sec.diam3d(i))
+        self.x, self.y, self.z = x, y, z
+    def _rotate_z(self, theta):
+        """Rotate the cell about the Z axis."""
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                x = sec.x3d(i)
+                y = sec.y3d(i)
+                c = h.cos(theta)
+                s = h.sin(theta)
+                xprime = x * c - y * s
+                yprime = x * s + y * c
+                sec.pt3dchange(i, xprime, yprime, sec.z3d(i), sec.diam3d(i))
 
 class BallAndStick(Cell):
     name = 'BallAndStick'
